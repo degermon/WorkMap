5de6c44a722a617bc395fcec8516850b0ca590f6
degermon <degermon@gmail.com>
2021-10-13 14:48:57 +0300

    Initial Commit

---
+++ WorkMap/WorkMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkMapApp.swift
+//  WorkMap
+//
+//  Created by Daniel Šuškevič on 2021-10-13.
+//
+
+import SwiftUI
+
+@main
+struct WorkMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
